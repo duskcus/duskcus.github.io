9488a0c4eeb2a7005b69d8296f188a92fec4dcc5
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-18 14:59:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-function myFunction() {
-    // JavaScript for collapsible functionality
-    document.getElementById('home').addEventListener('click', function() {
-      var childLinks = document.querySelector('.child-links');
-      childLinks.style.display = childLinks.style.display === 'block' ? 'none' : 'block';
-    });
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var homeLink = document.getElementById('home');
+  
+  homeLink.addEventListener('click', function() {
+    var childLinks = document.querySelector('.child-links');
+    childLinks.style.display = childLinks.style.display === 'block' ? 'none' : 'block';
+  });
+});
