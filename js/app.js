95d148fa0629d72aca8dc4885395d6c25eb80e0c
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-20 10:22:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
       // Get the target ID from the data attribute
       var targetId = this.getAttribute('data-target');
       var childLinks = document.getElementById(targetId);
-
       // Toggle the display of the corresponding child-links
       if (childLinks.style.display === 'block') {
         childLinks.style.display = 'none';
@@ -20,13 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
 // SETS NUMBERS FOR EACH LINE IN THE CODESNIPPET
 document.addEventListener("DOMContentLoaded", function() {
   const codeSnippets = document.querySelectorAll(".codesnippet-wrapper");
-
   codeSnippets.forEach(function(wrapper) {
     const codeElement = wrapper.querySelector(".codesnippet code");
     const lineNumberElement = wrapper.querySelector(".line-numbers");
-
     const codeLines = codeElement.innerText.split('\n').length;
-    
     for (let i = 1; i <= codeLines; i++) {
       const lineNumber = document.createElement("span");
       lineNumber.textContent = i;
@@ -41,11 +37,9 @@ const navSlide = () => {
     const Burger = document.querySelector('.burger');
     const Nav = document.querySelector('.sidebar'); // Target sidebar for toggling
     const NavLinks = document.querySelectorAll('.sidebar a');
-    
     Burger.addEventListener('click', () => {
         // Toggle sidebar visibility
         Nav.classList.toggle('nav-active');
-        
         // Animate the links
         NavLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -54,11 +48,9 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 + 0.5}s`;
             }
         });
-
         // Animate burger icon
         Burger.classList.toggle('toggle');
     });
 }
-
 navSlide();
 
